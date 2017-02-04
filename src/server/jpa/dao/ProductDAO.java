941c526bbb93f5ec50d57b8109fac1701adb45de
Arsenii Kuteinitsyn <arsenii.kuteinitsyn@gmail.com>
2017-02-04 23:19:19 +0400

    Added websocket functional

--- src/server/jpa/dao/ProductDAO.java
+++ src/server/jpa/dao/ProductDAO.java
@@ -1,43 +1,88 @@
 package server.jpa.dao;
 
-import server.ifaces.Manager;
+import com.google.gson.Gson;
 import server.jpa.entity.Product;
 
 import javax.ejb.Stateless;
+import javax.json.*;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.criteria.CriteriaQuery;
+import javax.websocket.OnClose;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.ServerEndpoint;
+import java.io.IOException;
 import java.io.Serializable;
+import java.io.StringReader;
 import java.util.List;
 
 /**
  * @author Arsenii
  */
-@Stateless(mappedName="ProductBean", name = "ProductBean")
-public class ProductDAO implements Manager, Serializable {
+@ServerEndpoint("/Product")
+@Stateless(mappedName = "ProductBean", name = "ProductBean")
+public class ProductDAO implements Serializable {
 
     @PersistenceContext(unitName = "NewPersistenceUnit")
     EntityManager em;
 
-    @Override
+    @OnMessage
+    public void onMessage(String message, Session session) throws IOException, InterruptedException {
+
+        JsonReader jsonReader = Json.createReader(new StringReader(message));
+        JsonObject messageJson = jsonReader.readObject();
+        String act = messageJson.getString("action");
+        switch (act) {
+
+            case "delete": {
+                delete(getById(messageJson.getInt("id")));
+                break;
+            }
+            case "add": {
+                String obj = messageJson.getString("object");
+                Gson gson = new Gson();
+                Product p = gson.fromJson(obj, Product.class);
+                add(p);
+                break;
+            }
+            case "getAll": {
+                List<Product> allProducts = getAll();
+                Gson gson = new Gson();
+                JsonObject out = Json.createObjectBuilder().add("action", "getAll").add("objects", gson.toJson(allProducts)).build();
+                session.getBasicRemote().sendText(out.toString());
+                break;
+            }
+        }
+    }
+
     public Product getById(Integer i) {
         return em.find(Product.class, i);
     }
 
-    @Override
     public List<Product> getAll() {
         CriteriaQuery cq = em.getCriteriaBuilder().createQuery();
         cq.select(cq.from(Product.class));
         return em.createQuery(cq).getResultList();
     }
 
-    @Override
     public void add(Product o) {
         em.merge(o);
     }
 
     public void delete(Product o) {
         em.remove(em.merge(o));
+    }
+
+    @OnOpen
+    public void onOpen() {
+        System.out.println("Client connected");
+    }
+
+    @OnClose
+    public void onClose() {
+        System.out.println("Connection closed");
     }
 
 }
